bc8c9375f82d1f199c5e16e21ae806e9a96fa977
dev-sas002 <dev-sas002@users.noreply.github.com>
2026-03-14 11:42:01 +0500

    Configure linting, formatting and build tooling

---
+++ outcome-service/jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  testEnvironment: 'node',
+  globalSetup: '<rootDir>/tests/globalSetup.js',
+  globalTeardown: '<rootDir>/tests/globalTeardown.js',
+  testMatch: ['<rootDir>/tests/**/*.test.js'],
+  testTimeout: 30000,
+  restoreMocks: true,
+};
